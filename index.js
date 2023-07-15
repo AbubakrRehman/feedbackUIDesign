809e302e877b6feb8a470777139ba9657b8d8514
AbubakrRehman <abubakr.rehman91@outlook.com>
2023-07-15 15:36:34 +0530

    dynmcally collcted rating txt n put in feedback UI

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 const ratings__container = document.getElementById("ratings__container");
 const ratingElements = document.querySelectorAll(".rating");
 const sendBtn = document.getElementById("send");
-const main__panel = document.getElementById("main__panel")
+const main__panel = document.getElementById("main__panel");
+let selectedRating = 'Satisfied';
 
 ratings__container.addEventListener("click", (e) => {
-    removeActiveClasses();
+
     if (e.target.parentNode.classList.contains("rating")) {
+        removeActiveClasses();
         e.target.parentNode.classList.add("active");
+        selectedRating = e.target.nextElementSibling.innerText;
     }
 })
 
 sendBtn.addEventListener("click", (e) => {
+
     main__panel.innerHTML = `<i class="feedback__text fa-solid fa-heart"></i>
     <p class="feedback__text">Thank You!</p>
-    <p class="feedback__text">Feedback: Neutral</p>
+    <p class="feedback__text">Feedback: ${selectedRating}</p>
     <p class="feedback__text" >We'll use your feedback to imrove our customer support</p>`;
 })
 
